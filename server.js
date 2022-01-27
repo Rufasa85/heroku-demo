9090d30b1e9734b41dbb6badfc48333580da8726
Joe Rehfuss <joe.rehfuss@gmail.com>
2022-01-27 14:17:50 -0800

    updates

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get("/randomnum",(req,res)=>{
+  res.json({
+    num:Math.floor(Math.random()*100)+1
+  })
+})
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
